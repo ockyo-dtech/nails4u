0d2de5cd010579cdcf394c871ab7138476709908
ockyo-tech <nguyentheduy.forwork@gmail.com>
2024-12-07 00:18:58 +0700

    fix: div-content

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
-  const Contact = () => <h1>Contact Page</h1>;
   return (
     <div className="App">
       <Router>
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
+        <div className="app-container">
+          <Header />
+          <div className="content">
+            <Home />s
+          </div>
+          <Footer />
         </div>
-
-        <Footer />
       </Router>
     </div>
   );

54e4b595f6062b872850ed14d821c4401f397bfd
ockyo-dtech <thduy.0605@gmail.com>
2024-12-09 22:29:34 +0700

    fix: config route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import About from './pages/About/About';
 import './App.css';
 
 function App() {
@@ -12,7 +13,11 @@ function App() {
         <div className="app-container">
           <Header />
           <div className="content">
-            <Home />s
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              {/* <Route path="/contact" element={<Contact />} /> */}
+            </Routes>
           </div>
           <Footer />
         </div>
